9f7fc6a06baa81262097cab0cb53dfbd30590e04
jirenz <zhujiren1155@hotmail.com>
2018-10-16 14:33:29 -0700

    fix read

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import os
 from setuptools import setup, find_packages
 
 
-# def read(fname):
-#     with open(os.path.join(os.path.dirname(__file__), fname)) as f:
-#         return f.read().strip()
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read().strip()
 
 setup(
     name='caraml',
